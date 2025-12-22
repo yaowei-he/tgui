7ec8e60adf14ed7d94ce003a042326741843f9e2
heyaowei9 <heyaowei9@gmail.com>
2025-12-22 10:18:12 +0000

    t3

--- src/components/PinSection.tsx
+++ src/components/PinSection.tsx
@@ -1,37 +1,24 @@
 import { useEffect, useState } from 'react';
 import { PinInput, Section } from '@telegram-apps/telegram-ui';
-import {
-  initData,
-  useSignal
-} from '@tma.js/sdk-react';
+
 import { useNavigate } from 'react-router-dom';
 
 export function PinSection() {
   const [code, setCode] = useState<number[]>([]);
-  const initDataState = useSignal(initData.state);
   const navigate = useNavigate();
 
 
   const handleSubmit = () => {
-    if (!initDataState?.user) {
-      console.log('Telegram user not found');
-      return;
-    }
+
 
     if (code.length !== 6) {
       console.log('PIN not complete');
       return;
     }
+    const pinString = code.join('')
 
-    const payload = {
-      telegramUserId: initDataState.user.id,
-      code,                      // number[]
-      pinString: code.join(''),  // 业务层使用
-      initDataRaw: initDataState, // ✅ 后端校验必需
-    };
 
-    console.log('发送数据:', payload);
-    // console.log("pin码 :" + code.join);
+    console.log("pin码 :" + pinString);
     navigate('/password');
 
     // await fetch('/api/verify-pin', {
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,11 @@ import './index.css';
 
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import { init } from '@tma.js/sdk-react';
+
 import { setBackgroundAsSecondary } from './helpers/setBackgroundAsSecondary';
 
 setBackgroundAsSecondary();
-init();
+
 const root = createRoot(
   document.getElementById('root') as HTMLElement
 );
